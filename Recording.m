638448c8f479e3604ba3155726fb00154f971ca4
Frank Midgley <midgleyf@janelia.hhmi.org>
2011-07-22 15:46:59 +0000

    Now supporting .daq files.

--- Recording.m
+++ Recording.m
@@ -36,6 +36,22 @@ classdef Recording < handle
                 obj.data = [];
                 obj.sampleRate = get(obj.videoReader, 'FrameRate');
                 obj.duration = get(obj.videoReader, 'Duration');
+            elseif strcmp(ext, '.daq')
+                info = daqread(filePath, 'info');
+                
+                % Ask the user which channel to open.
+                [channel, ok] = listdlg('ListString', cellfun(@(x)num2str(x), {info.ObjInfo.Channel.Index}'), ...
+                                        'PromptString', {'Choose the channel to open:', '(1-8: optical, 9: acoustic)'}, ...
+                                        'SelectionMode', 'single', ...
+                                        'Name', 'Open DAQ File');
+                if ok
+                    obj.isAudio = true;
+                    obj.sampleRate = info.ObjInfo.SampleRate;
+                    obj.data = daqread(filePath, 'Channels', channel);
+                    obj.duration = length(obj.data) / obj.sampleRate;
+                else
+                    obj = Recording.empty();
+                end
             end
         end
         
