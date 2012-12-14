68f38fb846ad9ad13e1d7e6231aa365c580e4939
Frank Midgley <midgleyf@janelia.hhmi.org>
2012-12-14 14:18:12 +0000

    Primary time indicators now always show minutes.

--- secondstr.m
+++ secondstr.m
@@ -1,12 +1,15 @@
-function s = secondstr(seconds, format)
+function s = secondstr(seconds, format, minFields)
+    if nargin == 2
+        minFields = 0;
+    end
     switch format
         case 0
             s = sprintf('%.2f', seconds);
         case 1
-            if seconds < 60
+            if seconds < 60 && minFields < 2
                 % seconds
                 s = sprintf('%.2f', seconds);
-            elseif seconds < 60 * 60
+            elseif seconds < 60 * 60 && minFields < 3
                 % minutes:seconds
                 s = sprintf('%d:%05.2f', floor(seconds / 60), mod(seconds, 60));
             elseif seconds < 60 * 60 * 24
