4ff6bf0d6c22a43d1be57ea210a2ac9b84940ad9
Ben Arthur <arthurb@janelia.hhmi.org>
2013-07-05 17:14:21 -0400

    debugged stern's .bin audio files

--- Recordings/SternRecording.m
+++ Recordings/SternRecording.m
@@ -1,12 +1,16 @@
 classdef SternRecording < AudioRecording
     
+    properties
+      channel
+    end
+    
     methods (Static)
         function canLoad = canLoadFromPath(filePath)
             % See if the file's extension is .bin.
             [~, ~, fileExt] = fileparts(filePath);
             canLoad = strcmp(fileExt, '.bin');
             if (canLoad)
-                fid = fopen(obj.filePath, 'r');
+                fid = fopen(filePath, 'r');
                 try
                     version = fread(fid, 1, 'double');
                     canLoad = (version == 1);
@@ -42,7 +46,7 @@ classdef SternRecording < AudioRecording
             if isempty(obj.channel)
                 fid = fopen(obj.filePath, 'r');
                 try
-                    fread(fid, 1, 'double');    % skip over the sample rate
+                    fread(fid, 2, 'double');    % skip over the version # and sample rate
                     nchan = fread(fid, 1, 'double');
                     [obj.channel, ok] = listdlg('ListString', cellstr(num2str((1:nchan)')), ...
                                                 'PromptString', {'Choose the channel to open:'}, ...
@@ -64,10 +68,11 @@ classdef SternRecording < AudioRecording
         function loadData(obj)
             fid = fopen(obj.filePath, 'r');
             try
+                fread(fid, 1, 'double');    % skip over version #
                 obj.sampleRate = fread(fid, 1, 'double');
                 nchan = fread(fid, 1, 'double');
-                fread(fid, obj.channel, 'double');  % skip over first timestamp and first channels
-                obj.data = fread(fid, inf, 'double', 8*(nchan-1));
+                fread(fid, obj.channel-1, 'double');  % skip over first channels
+                [obj.data, obj.sampleCount] = fread(fid, inf, 'double', 8*(nchan-1));
                 obj.name = sprintf('%s (channel %d)', obj.name, obj.channel);
                 fclose(fid);
             catch ME
