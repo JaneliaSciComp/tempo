77e55098f0c3d6ee043c8eb5cee6855fb602bfea
Frank Midgley <midgleyf@janelia.hhmi.org>
2014-01-27 14:53:40 -0500

    Better help for +UFMF and +SBFMF packages.

---
+++ Recordings/SBFMFRecording/+SBFMF/Contents.m
@@ -0,0 +1,8 @@
+% SBFMF Package
+% Version 1.0 27-Jan-2014
+% 
+% SBFMFFile                      - Class representing a SBFMF file.
+% isSBFMFFile                    - Test if the file at the indicated path is in SBFMF format.
+% openFile                       - Open an existing SBFMF file.
+% 
+% Copyright 2013-2014 Howard Hughes Medical Institute
