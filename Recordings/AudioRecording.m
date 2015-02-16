f1923d673600338656694e611f227e318cfb678c
Ben Arthur <arthurb@hhmi.org>
2015-02-16 12:11:21 -0500

    AudioRecordings are now buffered such that a subset can be loaded

--- Recordings/AudioRecording.m
+++ Recordings/AudioRecording.m
@@ -1,5 +1,13 @@
 classdef AudioRecording < Recording
     
+    properties (Constant)
+        bufferSize = 16 * 1024 * 1024;  % The maximum number of samples to load at any time.
+    end
+    
+    properties (Transient)
+        dataStartSample = -Inf
+    end
+    
     properties
         maxAmp
         channel
@@ -75,13 +83,59 @@ classdef AudioRecording < Recording
         function loadData(obj)
             if ~isempty(obj.filePath)
                 if isempty(which('audioread'))
-                    [obj.data, obj.sampleRate] = wavread(obj.filePath, 'native');
+                    info = wavread(obj.filePath, 'size');
+                    obj.sampleCount = info(1);
                 else
-                    [obj.data, obj.sampleRate] = audioread(obj.filePath, 'native');
+                    info = audioinfo(obj.filePath);
+                    obj.sampleCount = info.TotalSamples;
                 end
-                obj.data = double(obj.data(:,obj.channel));
-                obj.sampleCount = length(obj.data);
+                obj.loadDataBuffer(1);
             end
+        end
+        
+
+        function loadDataBuffer(obj, startSample)
+            startSample = max([1, min([startSample, obj.sampleCount - obj.bufferSize])]);
+            
+            if startSample ~= obj.dataStartSample
+                % Only read the data that we don't already have.
+                endSample = startSample + obj.bufferSize;
+                if startSample < obj.dataStartSample || startSample > obj.dataStartSample + obj.bufferSize
+                    readStart = startSample;
+                else
+                    readStart = obj.dataStartSample + obj.bufferSize;
+                end
+                if endSample < obj.dataStartSample || endSample > obj.dataStartSample + obj.bufferSize
+                    readEnd = endSample;
+                else
+                    readEnd = obj.dataStartSample;
+                end
+                readLength = readEnd - readStart;
+                newData = obj.readData(readStart, readLength);
+                
+                % Piece together the new data buffer from what we already had and what was just read in.
+                oldDataSize = (obj.bufferSize - readLength);
+                if readStart > startSample
+                    obj.data = [obj.data((end - oldDataSize + 1):end); newData];
+                elseif readEnd < endSample
+                    obj.data = [newData; obj.data(1:oldDataSize)];
+                else
+                    obj.data = newData;
+                end
+                
+                % Remember which chunk we've got loaded.
+                obj.dataStartSample = startSample;
+            end
+        end
+        
+
+        function newData = readData(obj, readStart, readLength)
+            if isempty(which('audioread'))
+                [data, obj.sampleRate] = wavread(obj.filePath, [readStart readStart+readLength], 'native');
+            else
+                [data, obj.sampleRate] = audioread(obj.filePath, [readStart readStart+readLength], 'native');
+            end
+            newData = double(data(:,obj.channel));
         end
         
         
@@ -100,17 +154,25 @@ classdef AudioRecording < Recording
         
         
         function [data, offset] = dataInTimeRange(obj, timeRange)
-            % If the returned data doesn't begin at the start of the time range then a non-zero offset will be returned.
-            
             fullRange = floor((timeRange + obj.timeOffset) * obj.sampleRate);
             sampleRange = fullRange;
             if sampleRange(1) < 1
                 sampleRange(1) = 1;
             end
-            if sampleRange(2) > length(obj.data)
-                sampleRange(2) = length(obj.data);
+            if sampleRange(2) > obj.sampleCount
+                sampleRange(2) = obj.sampleCount;
             end
-            data = obj.data(sampleRange(1):sampleRange(2));
+            
+            if sampleRange(2) - sampleRange(1) > obj.bufferSize
+                % The requested range is too big to load in all at once.
+                data = [];
+            else
+                if sampleRange(1) < obj.dataStartSample || sampleRange(2) > obj.dataStartSample + obj.bufferSize
+                    % At least some of the requested data is not currently loaded.
+                    obj.loadDataBuffer(sampleRange(1));
+                end
+                data = obj.data((sampleRange(1)) - obj.dataStartSample + 1:(sampleRange(2) - obj.dataStartSample));
+            end
             offset = (sampleRange(1) - fullRange(1)) / obj.sampleRate;
         end
         
